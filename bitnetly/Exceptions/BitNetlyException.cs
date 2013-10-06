3ebd9e9fd39d511c2d44bc45bf12c53257c63748
Lucas Brendel <lucasbrendel@live.com>
2013-10-06 13:32:28 -0500

    Added new classes

---
+++ bitnetly/Exceptions/BitNetlyException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace BitNetly.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BitNetlyException : Exception
+    {
+        private string status;
+
+        private string text;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Status
+        {
+            get { return status; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string StatusText
+        {
+            get { return text; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="text"></param>
+        public BitNetlyException(string status, string text)
+        {
+            this.status = status;
+            this.text = text;
+        }
+    }
+}
---
+++ bitnetly/Objects/Link.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitNetly.Objects
+{
+    public class Link
+    {
+        public Link()
+        {
+
+        }
+
+        public Link(string url)
+        {
+
+        }
+
+        public void Expand()
+        {
+
+        }
+
+        public void Info()
+        {
+
+        }
+
+        public void Shorten()
+        {
+
+        }
+
+        public void Lookup()
+        {
+
+        }
+
+        public void LinkEdit()
+        {
+
+        }
+
+        public void LinkSave()
+        {
+
+        }
+    }
+}
