24dd5f31204af595db40d114417b9a7987ecdaf8
Lucas Brendel <lucasbrendel@live.com>
2013-10-06 21:56:22 -0500

    Login using Basic OAuth

--- BitNetlyTester/Program.cs
+++ BitNetlyTester/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BitNetly.Objects;
+using BitNetly;
 
 namespace BitNetlyTester
 {
@@ -11,7 +12,9 @@ namespace BitNetlyTester
     {
         static void Main(string[] args)
         {
-            string l = Link.ShortenURL("http://google.com", "25d28cd775edccdb0e26cb2b4f79b8ffc78ff900");
+            //string l = Link.ShortenURL("http://google.com", "25d28cd775edccdb0e26cb2b4f79b8ffc78ff900");
+
+            BitNetlyService s = new BitNetlyService("o_5kbb1rm8hg", "winph820", "575d16b79a3f0afb5b8919e562a769107ccd4ec5", "072f9d8e5dba4d703ef5b54fc219af5bb21f882e", BitNetlyService.LoginRoute.Basic);
         }
     }
 }
--- bitnetly/BitNetlyService.cs
+++ bitnetly/BitNetlyService.cs
@@ -1,9 +1,21 @@
-﻿
+﻿using RestSharp;
+using System;
+using System.Web;
+using RestSharp.Authenticators;
+
 namespace BitNetly
 {
     public class BitNetlyService
     {
         private string accessToken;
+        private string clientID;
+        private string clientSecret;
+
+        public enum LoginRoute
+        {
+            Basic,
+            OAuth2
+        };
 
         /// <summary>
         /// 
@@ -15,12 +27,71 @@ namespace BitNetly
         /// </summary>
         public const string APIURL = "https://api-ssl.bitly.com/";
 
+        public const string OAUTHURL = "https://bitly.com/";
+
         /// <summary>
         /// 
         /// </summary>
         public BitNetlyService(string accessToken)
         {
             this.accessToken = accessToken;
+        }
+
+        public BitNetlyService(string username, string password, string clientID, string clientSecret, LoginRoute route)
+        {
+            this.clientID = clientID;
+            this.clientSecret = clientSecret;
+            switch(route)
+            {
+                case LoginRoute.Basic:
+                    this.accessToken = this.BasicAuthLogin(username, password);
+                    break;
+                case LoginRoute.OAuth2:
+                    this.accessToken = this.OAuth2Login(username, password);
+                    break;
+            }
+        }
+
+        private string BasicAuthLogin(string username, string password)
+        {
+            string token = String.Empty;
+
+            RestClient c = new RestClient(APIURL);
+            c.Authenticator = new HttpBasicAuthenticator(username, password);
+            RestRequest r = new RestRequest("/oauth/access_token", Method.POST);
+            //r.AddParameter("grant_type", "password");
+            r.AddParameter("client_id", this.clientID);
+            r.AddParameter("client_secret", this.clientSecret);
+            r.AddParameter("Content-Type", "application/x-www-form-urlencoded");
+            string keys = String.Format("{0}:{1}", username, password);
+            r.AddHeader("Authorization", "Basic " + EncodeTo64(keys));
+            RestResponse i = (RestResponse)c.Execute(r);
+
+            if (i.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                token = i.Content;
+            }
+
+            return token;
+        }
+
+        private string OAuth2Login(string username, string password)
+        {
+            string token = String.Empty;
+
+            RestClient c = new RestClient(OAUTHURL);
+            RestRequest r = new RestRequest("oauth/authorize");
+
+            return token;
+        }
+
+        private string EncodeTo64(string code)
+        {
+            byte[] toEncodeAsBytes
+              = System.Text.ASCIIEncoding.ASCII.GetBytes(code);
+            string returnValue
+                  = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
         }
     }
 }
