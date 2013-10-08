bd07d10c8e3ff0ffb6b4c184f41ec9bea8fd0698
Lucas Brendel <lucasbrendel@live.com>
2013-10-08 06:39:02 -0500

    removed sensitive testing info

--- BitNetlyTester/Program.cs
+++ BitNetlyTester/Program.cs
@@ -12,11 +12,7 @@ namespace BitNetlyTester
     {
         static void Main(string[] args)
         {
-            //string l = Link.ShortenURL("http://google.com", "25d28cd775edccdb0e26cb2b4f79b8ffc78ff900");
-
-            BitNetlyService s = new BitNetlyService("o_5kbb1rm8hg", "winph820", "575d16b79a3f0afb5b8919e562a769107ccd4ec5", "072f9d8e5dba4d703ef5b54fc219af5bb21f882e", BitNetlyService.LoginRoute.Basic);
-
-            Link l = Link.ExpandURL("http://bit.ly/lucasandsara", s.AccessToken);
+            
         }
     }
 }
