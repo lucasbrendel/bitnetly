3a8f58980778e53d44c74d2cf88a4aaf7dc6b655
Lucas Brendel <lucasbrendel@live.com>
2013-11-17 17:57:33 -0600

    Renamed project file names and removed the tester project.

--- Src/bitnetly/Properties/AssemblyInfo.cs
+++ Src/bitnetly/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("bitnetly")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bitnetly")]
+[assembly: AssemblyDescription("A Windows Phone Library for accessing the Bit.ly API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("bitnetly")]
+[assembly: AssemblyProduct("Bitnetly")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
